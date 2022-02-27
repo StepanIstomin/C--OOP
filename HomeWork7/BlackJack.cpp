e2b540994746bc610808fd39bc77b7debd25c219
Stepan <Steven.nnov@gmail.com>
2022-02-27 21:48:33 +0300

    HomeWork 7 files added

---
+++ HomeWork7/BlackJack.cpp
@@ -0,0 +1,282 @@
+﻿//  C++ OOP. BlackJack game
+// Author: Stepan Istomin
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <Windows.h> //for SetConsole
+#include <algorithm> // for shuffle
+#define DELAY 1000
+
+enum Suit {
+	DIAMONDS,	// Бубны
+	CLUBS,		// Трефы
+	HEARTS,		// Червы
+	SPADES		// Пики
+};
+enum CardName {
+	ACE = 1,
+	TWO,
+	THREE,
+	FOUR,
+	FIVE,
+	SIX,
+	SEVEN,
+	EIGHT,
+	NINE,
+	TEN,
+	JACK,
+	QUEEN,
+	KING
+};
+class Card
+{
+public:
+	Card(Suit _suit, CardName _name) : suit(_suit), name(_name) {};
+	void Flip() {
+		state = !state;
+	}
+	int GetValue() {
+		if (!state) return 0;
+		else if (name < 11) return name;
+		else return 10;
+	}
+	friend std::ostream& operator<<(std::ostream& os, const Card& _Card); // task 5
+private:
+	Suit suit;
+	CardName name;
+	bool state = 1; // 0 - card face down, 1 - card face up
+};
+std::ostream& operator<<(std::ostream& os, const Card& _Card)
+{
+	const std::string RANKS[] = { "0", "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+	const std::string SUITS[] = { u8"\u2666", u8"\u2663", u8"\u2665", u8"\u2660" };
+	SetConsoleOutputCP(CP_UTF8);
+	if (_Card.state)
+	{
+		os << RANKS[_Card.name] << SUITS[_Card.suit];
+	}
+	else
+	{
+		os << "XX";
+	}
+	return os;
+}
+
+class Hand
+{
+public:
+	Hand() {};
+	void Add(Card* _card) { hand.push_back(_card); }
+	void Clear() {
+		std::vector<Card*>::iterator iter = hand.begin();
+		for (iter = hand.begin(); iter != hand.end(); ++iter)
+		{
+			delete* iter;
+			*iter = 0;
+		}
+		hand.clear();
+	}
+	int GetValue() const {
+		if (hand.empty())
+			return 0;
+		if (hand.size()) {
+			size_t sum = 0;
+			size_t aceCount = 0;
+			for (size_t i = 0; i < hand.size(); i++) //Calculating points without aces 
+			{
+				if (hand[i]->GetValue() == ACE) // If card is ace
+					aceCount++;
+				else
+					sum += hand[i]->GetValue(); // Any card other than an ace 
+			}
+			for (aceCount; aceCount > 0; ) // Algorithm for calculating points from aces 
+			{
+				sum += (sum + 11 <= 21 - --aceCount) ? 11 : 1;
+			}
+			return sum;
+		}
+		else
+			return 0;
+	}
+protected:
+	std::vector<Card*> hand;
+};
+class GenericPlayer : public Hand
+{
+public:
+	GenericPlayer(std::string _name) : name(_name) {};
+	virtual bool IsHitting() const = 0;
+	bool IsBoosted() {
+		if (GetValue() > 21) { return true; }
+		else { return false; }
+	}
+	void Bust() {
+		if (IsBoosted())
+			std::cout << name << " is busts!" << std::endl;
+	}
+	friend std::ostream& operator<<(std::ostream& os, const GenericPlayer& _GenPlayer); // task 5
+protected:
+	std::string name;
+};
+class Player : public GenericPlayer {
+public:
+	Player(std::string _name) : GenericPlayer(_name) {};
+	virtual bool IsHitting() const {
+		if (GetValue() == 21) return 0;
+		char ans;
+		do {
+			std::cout << "Do you want to take another card? (y/n)";
+			std::cin >> std::setw(1) >> ans;
+			std::cin.ignore(32767, '\n');
+		} while (ans != 'y' && ans != 'n' );
+		if (ans == 'y') return 1;
+		else return 0;
+	};
+	void Win() const { std::cout << name << " won!" << std::endl; }
+	void Lose() const { std::cout << name << " lost!" << std::endl; }
+	void Push() const { std::cout << name << " has tied." << std::endl; }
+};
+class House : public GenericPlayer
+{
+public:
+	House() : GenericPlayer("Dealer") {};
+	virtual bool IsHitting() const {
+		return (GetValue() < 17) ? 1 : 0;
+	}
+	void FlipFirstCard() {
+		hand[0]->Flip();
+	}
+};
+std::ostream& operator<<(std::ostream& os, const GenericPlayer& _GenPlayer)
+{
+	os << _GenPlayer.name << ": ";
+	for (size_t i = 0; i < _GenPlayer.hand.size(); i++)
+	{
+		os << *_GenPlayer.hand[i] << " ";
+	}
+	os << "(" << _GenPlayer.GetValue() << ")";
+	return os;
+}
+class Deck : public Hand
+{
+public:
+	Deck() { Populate(); };
+	void Populate(){
+		for (size_t i = 0; i < 4; i++)	{		//card suit
+			for (size_t j = 1; j <= 13; j++) {	//card name
+				Add(new Card(static_cast<Suit>(i), static_cast<CardName>(j)));
+			}
+		}
+	}
+	void Shuffle(){
+		std::random_shuffle(hand.begin(),hand.end());
+	}
+	void Deal(Hand& aHand){
+		if (!hand.empty()) {
+			aHand.Add(hand.back());
+			hand.pop_back();
+		}
+		else std::cout << "Out of cards. Unable to deal.";
+	}
+	void AddltionalCards(GenericPlayer& aGenerlcPlayer) {
+		while (!(aGenerlcPlayer.IsBoosted()) && aGenerlcPlayer.IsHitting())
+		{
+			Deal(aGenerlcPlayer);
+			std::cout << aGenerlcPlayer << std::endl;
+			Sleep(DELAY);
+			if (aGenerlcPlayer.IsBoosted())
+				aGenerlcPlayer.Bust();
+		}
+	}
+};
+class Game
+{
+public:
+	Game(std::vector<std::string> _names) {
+		std::vector<std::string>::const_iterator pName;
+		for (pName = _names.begin(); pName != _names.end(); ++pName)
+			players.push_back(Player(*pName));
+		deck.Populate();
+		srand(static_cast<unsigned int>(time(0)));
+		deck.Shuffle();
+	};
+	void play() {
+		// Start hand
+		std::vector<Player>::iterator pPlayer;
+		for (size_t i = 0; i < 2; i++) {
+			deck.Deal(house);
+			for (pPlayer = players.begin(); pPlayer != players.end(); ++pPlayer)
+				deck.Deal(*pPlayer);
+		}
+		house.FlipFirstCard();
+		std::cout << std:: endl << house << std::endl;
+		// Additional cards
+		for (pPlayer = players.begin(); pPlayer != players.end(); ++pPlayer)
+			std::cout << *pPlayer << std::endl;
+		for (pPlayer = players.begin(); pPlayer != players.end(); ++pPlayer)
+			deck.AddltionalCards(*pPlayer);
+		house.FlipFirstCard();
+		std::cout << house << std::endl;
+		Sleep(DELAY);
+		for (pPlayer = players.begin(); pPlayer != players.end(); ++pPlayer) { // If only one player
+			if (!pPlayer->IsBoosted())
+				deck.AddltionalCards(house);
+			else pPlayer->Lose();
+		}
+		// Win conditions
+		if (house.IsBoosted()) {
+			for (pPlayer = players.begin(); pPlayer != players.end(); ++pPlayer) {
+				if (!pPlayer->IsBoosted())
+					pPlayer->Win();
+			}
+		}
+		else {
+			for (pPlayer = players.begin(); pPlayer != players.end(); ++pPlayer) {
+				if (!pPlayer->IsBoosted()) {
+					if (pPlayer->GetValue() > house.GetValue())
+						pPlayer->Win();
+					else if (pPlayer->GetValue() < house.GetValue())
+						pPlayer->Lose();
+					else
+						pPlayer->Push();
+				}
+			}
+		}
+		//hand clearing
+		for (pPlayer = players.begin(); pPlayer != players.end(); ++pPlayer)
+			pPlayer->Clear();
+		house.Clear();
+	};
+	bool wantToPlay() {
+		char ans;
+		do {
+			std::cout << "Do you want to play again? (y/n)";
+			std::cin >> std::setw(1) >> ans;
+			std::cin.ignore(32767, '\n');
+		} while (ans != 'y' && ans != 'n');
+		if (ans == 'y') return 1;
+		else return 0;
+	}
+private:
+	Deck deck;
+	House house;
+	std::vector<Player> players;
+};
+int main()
+{
+	using namespace std;
+	cout << "Welcome to Blackjack game!" << endl;
+	bool playOn;
+	string playerName;
+	vector<string> names;
+	cout << "Enter your name: ";
+	cin >> playerName;
+	names.push_back(playerName);
+	do {
+		Game game(names);
+		game.play();
+		playOn = game.wantToPlay();
+	} while (playOn);
+}
---
+++ HomeWork7/HomeWork7.cpp
@@ -0,0 +1,79 @@
+﻿// C++ OOP. Lesson 7 homework
+// Author: Stepan Istomin
+
+#include <iostream>
+#include <ctime>
+#include <memory>
+
+// Task1
+class Date
+{
+public:
+	Date(signed short _day, signed short _month, signed short _year) : day(_day), month(_month), year (_year) {};
+	void changeDay(signed short _day) { day = _day; }
+	void changeMonth(signed short _month) { month = _month; }
+	void changeYear(signed short _year) { year = _year; }
+	friend std::ostream& operator<<(std::ostream& os, const Date& _Date);
+	friend Date laterDate(const std::shared_ptr<Date> _date1, const std::shared_ptr<Date> _date2);
+private:
+	signed short day;
+	signed short month;
+	signed short year;
+};
+std::ostream& operator<<(std::ostream& os, const Date& _Date)
+{
+	os << _Date.day << "." << _Date.month << "." << _Date.year;
+	return os;
+}
+// Task 2
+Date laterDate(const std::shared_ptr<Date> _date1, const std::shared_ptr<Date> _date2) {
+	if (_date1->year > _date2->year) return *_date1;
+	else if (_date1->year < _date2->year) return *_date2;
+	else {
+		if (_date1->month > _date2->month) return *_date1;
+		else if (_date1->month < _date2->month) return *_date2;
+		else {
+			if (_date1->day > _date2->day) return *_date1;
+			else return *_date2;
+		}
+	}
+}
+void swapDate(std::shared_ptr<Date> &_date1, std::shared_ptr<Date> &_date2) { //Обязательно передавать умный указатель по адресу, иначе передается копия
+	_date1.swap(_date2);
+}
+int main()
+{
+	using namespace std;
+    //task 1
+	{
+		//auto_ptr and scoped_ptr - old style 
+		// new C++11 smart pointers:
+		//unique_ptr - replace for auto_ptr
+		//weak_ptr - used when you need a smart pointer that has access to a resource but is not considered its owner 
+		//shared_ptr - used to own a single dynamically allocated resource by multiple smart pointers at once
+
+		//unique_ptr<Date> today(new Date(25,2,2022));
+		//unique_ptr<Date> date;
+		shared_ptr<Date> today(new Date(25, 2, 2022));
+		shared_ptr<Date> date;
+
+		today->changeDay(26);
+		today->changeYear(22);
+		cout << *today << endl;
+		date = today;
+		if (date.get()) cout << "date pointer is not null" << endl;
+		else cout << "date pointer is null" << endl;
+		if (today.get()) cout << "today pointer is not null" << endl;
+		else cout << "today pointer is null" << endl;
+	}
+	//task 2
+	{
+		shared_ptr<Date> date1(new Date(9, 11, 2005));
+		shared_ptr<Date> date2(new Date(8, 12, 2004));
+		cout << laterDate(date1, date2) << endl; // unique_ptr не передаются в функцию?
+		cout << *date2 << endl;
+		swapDate(date1, date2);
+		cout << *date1 << endl << *date2;
+	}
+}
+
